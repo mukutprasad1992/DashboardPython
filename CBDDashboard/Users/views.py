c9c13c15bfee32547a293348c31dbc1e65bf4c7f
unknown <Mukut.Prasad@eton-solutions.com>
2023-10-25 22:00:18 +0530

    Added status and message in user module

--- CBDDashboard/Users/views.py
+++ CBDDashboard/Users/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Users
 from .serializers import UserSerializer
 
@@ -6,6 +7,31 @@ class UserListCreateView(generics.ListCreateAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status': 'success',
+                'message': 'User created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': 'error',
+            'message': 'User creation failed',
+            'errors': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        users = Users.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response({
+            'status': 'success',
+            'message': 'Users retrieved successfully',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+
